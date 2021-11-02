21f3294d1411dc66798417d28ed75675f6845076
Serg <a_s_v86@mail.ru>
2021-11-02 12:36:28 +0300

    Initial

---
+++ ReadJson1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadThreeDifferentJsonStructure
+{
+    public interface IReadJson1
+    {
+        void Read(string json1);
+    }
+    public class ReadJson1 : IReadJson1
+    {
+        public void Read(string json1)
+        {
+            Console.WriteLine(json1);
+        }
+    }
+}
